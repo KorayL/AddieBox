0befb9a11ce8de7c495da098e3321c0b4151f0d2
KorayL <i.koray@me.com>
2022-12-13 14:02:33 -0500

    Update gitTest.py

--- gitTest.py
+++ gitTest.py
@@ -1,4 +1,6 @@
 from git import Repo
+import time
 
 repo = Repo("/home/pi/AddieBox")
-print(repo.head.commit.committed_date)
+print(f"Seconds since epoch of last commit: {repo.head.commit.committed_date}")
+
