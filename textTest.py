1cb9cf5c9ab638ebf434fec6dacb3de4d1c15e88
KorayL <i.koray@me.com>
2022-12-13 17:57:42 -0500

    Update textTest.py

--- textTest.py
+++ textTest.py
@@ -26,8 +26,8 @@ font = ImageFont.truetype(fontName, 15)
 draw = ImageDraw.Draw(image)
 
 # string = "In the loveliest town of all, where the houses were white and high and the elms trees were green and higher than the houses, where the front yards were wide and pleasant and the back yards were bushy and worth finding out about, where the streets sloped down to the stream and the stream flowed quietly under the bridge, where the lawns ended in orchards and the orchards ended in fields and the fields ended in pastures and the pastures climbed the hill and disappeared over the top toward the wonderful wide sky, in this loveliest of all towns Stuart stopped to"
-string = "Koray's Screen Works!"
-# string = "This is just a really long test of my string line return 'algorithm'. I really hope it works well!"
+# string = "Koray's Screen Works!"
+string = "This is just a really long test of my string line return 'algorithm'. I really hope it works well!"
 tokens = string.split()
 
 finalString = tokens[0]  # adds first word
