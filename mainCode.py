b73e8013f8c22919dbe3226ad2c526f124eca798
KorayL <i.koray@me.com>
2022-12-14 15:13:06 -0500

    Update mainCode.py

--- mainCode.py
+++ mainCode.py
@@ -53,17 +53,18 @@ def main():
         #     led.off()
 
         if tiltSwitch.is_pressed:
-                if not lastState:
-                    fileNumber = 0  # work with last uploaded file
-                    fileData = fetch_data()
-                    fileData[fileNumber].display(displayWidth, displayHeight, disp)
-                    # update_accessed_files(fileCount-fileNumber, accessedFiles)
-                if button.is_pressed:
-                    fileNumber += 1
-                    if fileNumber > len(fileData)-1:
-                        fileNumber = 0
-                    fileData[fileNumber].display(displayWidth, displayHeight, disp)
-                    # update_accessed_files(fileCount - fileNumber, accessedFiles)
+            if not lastState:
+                fileNumber = 0  # work with last uploaded file
+                fileData = fetch_data()
+                fileData[fileNumber].display(displayWidth, displayHeight, disp)
+                # update_accessed_files(fileCount-fileNumber, accessedFiles)
+            if button.is_pressed:
+                fileNumber += 1
+                if fileNumber > len(fileData)-1:
+                    fileNumber = 0
+                fileData[fileNumber].display(displayWidth, displayHeight, disp)
+                # update_accessed_files(fileCount - fileNumber, accessedFiles)
+            lastState = 1
         else:
             disp.image(blackImage)
             lastState = 0
