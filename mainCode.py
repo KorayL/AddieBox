eb6333b9dbded781772de8e1d210fd634d5665f2
KorayL <i.koray@me.com>
2022-12-15 22:33:55 -0500

    Update mainCode.py

--- mainCode.py
+++ mainCode.py
@@ -72,6 +72,7 @@ def main():
 
 def update_seen_files(fileNumber, seenFiles):
     if fileNumber not in seenFiles:
+        print(seenFiles)
         newSeenFiles = seenFiles.append(fileNumber).sort()
         seenFilesFile = open("AddieBox/accessedFiles.txt", "w")
         seenFilesFile.truncate(0)
