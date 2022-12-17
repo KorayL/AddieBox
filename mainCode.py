949cc300b06edc71dc658d2ef03bfac82a310074
KorayL <i.koray@me.com>
2022-12-16 22:27:16 -0500

    Update mainCode.py

--- mainCode.py
+++ mainCode.py
@@ -75,7 +75,7 @@ def main():
             lastState = 0
 
             # Update Data Files
-            if cloneLoopCounter >= 25_000:
+            if cloneLoopCounter >= 2_500:
                 shutil.rmtree("Addie-Box-Data")
                 print("repo removed")
                 Repo.clone_from("https://github.com/KorayL/Addie-Box-Data.git", "Addie-Box-Data")
